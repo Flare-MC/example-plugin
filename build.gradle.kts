095d1c6b5084aedc77e1d6db1f2c00affeec7313
Dani-error <zdani.error@gmail.com>
2024-09-29 20:26:24 +0100

    feat: base

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import com.flare.gradle.configuration.FlareExtension
+import com.flare.gradle.configuration.FlarePlatformType
+
+
+buildscript {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.flare.gradle:gradle-plugin:1.0.0-SNAPSHOT")
+    }
+}
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("com.flare.gradle") version "1.0.0-SNAPSHOT"
+}
+
+group = "com.flare"
+version = "1.0-SNAPSHOT"
+
+configure<FlareExtension> {
+    entrypoint = "com.flare.example.ExamplePlugin"
+    sdkVersion = "1.0.0-SNAPSHOT"
+    platforms = mapOf(
+        FlarePlatformType.SPIGOT to "org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT",
+        FlarePlatformType.VELOCITY to "com.velocitypowered:velocity-api:3.4.0-SNAPSHOT",
+        FlarePlatformType.BUNGEECORD to "net.md-5:bungeecord-api:1.21-R0.1-SNAPSHOT"
+    )
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(8)
+}
---
+++ src/main/kotlin/com/flare/example/ExamplePlugin.kt
@@ -0,0 +1,14 @@
+package com.flare.example
+
+/*
+ * Project: example-plugin
+ * Created at: 29/9/24 16:42
+ * Created by: Dani-error
+ */
+class ExamplePlugin {
+
+    fun onEnable() {
+
+    }
+
+}
