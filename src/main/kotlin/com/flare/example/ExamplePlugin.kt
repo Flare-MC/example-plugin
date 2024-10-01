e2b646af0de59017b057650f4d3ea9d9970f712b
Dani-error <zdani.error@gmail.com>
2024-10-01 20:17:11 +0100

    feat: more data types

--- src/main/kotlin/com/flare/example/ExamplePlugin.kt
+++ src/main/kotlin/com/flare/example/ExamplePlugin.kt
@@ -1,5 +1,12 @@
 package com.flare.example
 
+import com.flare.sdk.FlareConfiguration
+import com.flare.sdk.annotations.DataFolder
+import com.flare.sdk.annotations.PlatformAccessor
+import com.flare.sdk.annotations.PluginConfiguration
+import com.flare.sdk.platform.Platform
+import java.io.File
+
 /*
  * Project: example-plugin
  * Created at: 29/9/24 16:42
@@ -7,8 +14,25 @@ package com.flare.example
  */
 class ExamplePlugin {
 
+    @PlatformAccessor
+    private lateinit var platform: Platform
+
+    @DataFolder
+    private lateinit var dataFolder: File
+
+    @PluginConfiguration
+    private lateinit var configuration: FlareConfiguration
+
+    fun onLoad() {
+        println("Loading ${configuration.name} plugin...")
+    }
+
     fun onEnable() {
-        println("Hello, it works!")
+        println("Hello, it works! (Running on ${platform.platformType.name} - ${dataFolder.absolutePath})")
+    }
+
+    fun onDisable() {
+        println("Disabling plugin...")
     }
 
 }
